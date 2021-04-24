13ade34dd952850571a95f5d9eff29a72aef7c5d
LuanRamalho <32373552+LuanRamalho@users.noreply.github.com>
2021-04-24 13:53:59 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -2,148 +2,151 @@
 
 class Calculadora():
     def adição():
-        num = [0]*9999
         i = 1
-        z = 0
-        resultado = 0.00
         while(i>0):
+            print("Digite um número qualquer: ")
+            num1 = float(input())
             print("Digite 1 número qualquer: ")
-            num[z] = float(input())
+            num2 = float(input())
                         
-            resultado = resultado + num[z]
+            resultado =  num1 + num2
             print("")
             print("Resultado: ",resultado)
-            
-            i = i + 1
-            z = z + 1
-            
+
             print("")
+
+            i = i + 1
             print("Digite 1 para continuar ou 0 para sair")
             i = int (input())
-            
             print("")
         
         print("Resultado: ",resultado)
         
         
     def subtração():
-        num = [0]*9999
         i = 1
-        z = 0
-        resultado = 0.00
-        while(i>0):
-            print("Digite 1 número qualquer: ")
-            num[z] = float(input())
-                        
-            resultado = resultado - num[z]
+        while (i > 0):
+            print("Digite um número qualquer: ")
+            num1 = float(input())
+            print("Digite outro número qualquer: ")
+            num2 = float(input())
+
+            resultado = num1 - num2
             print("")
-            print("Resultado: ",resultado)
-            
+            print("Resultado: ", resultado)
+
+            print("")
+
             i = i + 1
-            z = z + 1
-            
-            print("")
             print("Digite 1 para continuar ou 0 para sair")
-            i = int (input())
-            
+            i = int(input())
             print("")
-        
-        print("Resultado: ",resultado)   
+
+        print("Resultado: ", resultado)
 
 
     def multiplicação():
-        num = [0]*9999
         i = 1
-        z = 0
-        resultado = 1.00
-        while(i>0):
-            print("Digite 1 número qualquer: ")
-            num[z] = float(input())
-                        
-            resultado = resultado * num[z]
+        while (i > 0):
+            print("Digite um número qualquer: ")
+            num1 = float(input())
+            print("Digite outro número qualquer: ")
+            num2 = float(input())
+
+            resultado = num1 * num2
             print("")
-            print("Resultado: ",resultado)
-            
+            print("Resultado: ", resultado)
+
+            print("")
+
             i = i + 1
-            z = z + 1
-            
-            print("")
             print("Digite 1 para continuar ou 0 para sair")
-            i = int (input())
-            
+            i = int(input())
             print("")
-        
-        print("Resultado: ",resultado)     
+
+        print("Resultado: ", resultado)
 
 
     def divisão():
-        divisor = [0]*9999
-        dividendo = [0]*9999
         i = 1
-        z = 0
-        while(i>0):
-            print("Digite o divisor: ")
-            divisor[z] = float(input())
-            print("Digite o dividendo: ")
-            dividendo[z] = float(input())
-                        
-            resultado = divisor[z] / dividendo[z]
+        while (i > 0):
+            print("Digite um número qualquer: ")
+            num1 = float(input())
+            print("Digite outro número qualquer: ")
+            num2 = float(input())
+
+            resultado = num1 / num2
             print("")
-            print("Resultado: ",resultado)
-            
+            print("Resultado: ", resultado)
+
+            print("")
+
             i = i + 1
-            z = z + 1
-            
-            print("")
             print("Digite 1 para continuar ou 0 para sair")
-            i = int (input())
-            
-            print("")   
+            i = int(input())
+            print("")
+
+        print("Resultado: ", resultado)
         
     def exponenciação():
-        base = [0]*9999
-        expo = [0]*9999
         i = 1
-        z = 0
         while(i>0):
             print("Digite a base: ")
-            base[z] = float(input())
+            base = float(input())
             print("Digite o expoente: ")
-            expo[z] = float(input())
+            expo = float(input())
                         
-            resultado = base[z] ** expo[z]
+            resultado = base ** expo
             print("")
             print("Resultado: ",resultado)
-            
-            i = i + 1
-            z = z + 1
-            
+
             print("")
+
+            i = i + 1
+            print("Digite 1 para continuar ou 0 para sair")
+            i = int (input())
+            print("")
+
+        print("Resultado: ", resultado)
+
+    def raiz_quadrada():
+        i = 1
+        while(i>0):
+            print("Digite 1 número qualquer: ")
+            num = float(input())
+            resultado =  num ** (1/2)
+            print("")
+            print("Resultado: ",resultado)
+
+            print("")
+
+            i = i + 1
             print("Digite 1 para continuar ou 0 para sair")
             i = int (input())
             
             print("")
 
-    def raiz_quadrada():
-        num = [0]*9999
+        print("Resultado: ", resultado)
+
+    def raiz_cúbica():
         i = 1
-        z = 0
-        while(i>0):
+        while (i > 0):
             print("Digite 1 número qualquer: ")
-            num[z] = float(input())
-                        
-            resultado =  num[z] ** (1/2)
+            num = float(input())
+            resultado = num ** (1/3)
             print("")
-            print("Resultado: ",resultado)
-            
+            print("Resultado: ", resultado)
+
+            print("")
+
             i = i + 1
-            z = z + 1
-            
-            print("")
+
             print("Digite 1 para continuar ou 0 para sair")
-            i = int (input())
-            
+            i = int(input())
+
             print("")
+
+        print("Resultado: ", resultado)
             
     def fatorial():
         resultado = 1
@@ -160,7 +163,7 @@ class Calculadora():
         print("")
         print("Fatorial: ",resultado)
         
-    def porcentagem():
+    def porcentagem_1():
         i = 1
         while(i>0):
             print("Digite o primeiro número qualquer")
@@ -172,15 +175,51 @@ class Calculadora():
             
             print("")
             
-            print("Resultado: ",porcentagem,"%")
+            print("Resultado: %.2f" % porcentagem,"%")
             
             print("")
             print("")
-            
+
             i = i + 1
+
             print("Digite 1 para continuar ou 0 para sair")
             i = int (input())
-        
+
+        print("Resultado: %.2f" % porcentagem, "%")
+
+    def porcentagem_2():
+        i = 1
+        while (i > 0):
+            num = float(input("Digite um número qualquer: "))
+            P = float(input("Digite o valor Acréscimo/Descrécimo percentual: "))
+            R = num + (num * P) / 100
+
+            print("")
+            print("Resultado: %.0f" % R)
+
+            print("")
+            print("")
+            print("Digite 1 para continuar ou 0 para sair: ")
+            i = int(input())
+
+        print("Resultado: %.0f" % R)
+
+    def porcentagem_3():
+        i = 1
+        while (i > 0):
+            num = float(input("Digite um número: "))
+            P = float(input("Digite um valor percentual: "))
+            R = (num * P) / 100
+
+            print("")
+            print("Resultado: %.0f" % R)
+
+            print("")
+            print("")
+            print("Digite 1 para continuar ou 0 para sair: ")
+            i = int(input())
+
+        print("Resultado: %.0f" % R)
 
 c = Calculadora
 
@@ -192,8 +231,11 @@ print("Digite 3 para Multiplicação")
 print("Digite 4 para Divisão")
 print("Digite 5 para Exponenciação")
 print("Digite 6 para Raíz Quadrada")
-print("Digite 7 para Fatorial")
-print("Digite 8 para Porcentagem")
+print("Digite 7 para Raiz Cúbica")
+print("Digite 8 para Fatorial")
+print("Digite 9 para Porcentagem 1")
+print("Digite 10 para Porcentagem 2")
+print("Digite 11 para Porcentagem 3")
 
 op = int(input())
 if (op==1):
@@ -209,7 +251,14 @@ elif (op==5):
 elif (op==6):
     c.raiz_quadrada()
 elif (op==7):
-    c.fatorial()
+    c.raiz_cúbica()
 elif (op==8):
-    c.porcentagem()
-    
+    c.fatorial()
+elif (op==9):
+    c.porcentagem_1()
+elif (op==10):
+    c.porcentagem_2()
+elif (op==11):
+    c.porcentagem_3()
+
+input()
